24428002ba636a4d0ef3768c8558e13f333a4ad5
lyanbrito <lyanbrito613@gmail.com>
2024-03-23 13:48:52 -0300

    feat:crated hidden navbar

---
+++ src/components/hidden-navbar/script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("side-nav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("side-nav").style.width = "0";
+  }
