5de73bba3a48007e7d7475b4acc715edd0c57f43
lucascorreaa <lucas.lca14@gmail.com>
2024-03-23 08:27:35 -0300

    feat: added component hamburguer menu

---
+++ src/components/hamburger-menu/script.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById("menu").style
+
+function handleMenu() {
+    if (menu.display === "block") {
+      menu.display = "none";
+    } else {
+      menu.display = "block";
+    }
+  }
